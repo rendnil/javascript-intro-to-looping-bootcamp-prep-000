e0b02251c4c9db54dac4e8008a9be9fd930b93a9
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-03-08 02:09:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -34,4 +34,15 @@ function whileLoop(number){
 function doWhileLoop(array) {
   
   
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do{
+  remove;
+  
+}while array.length> 0 && maybeTrue();
+
+return array
+
 }
